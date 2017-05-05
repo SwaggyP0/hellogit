741f16e2659344fb6b5f5e56cf56f6536859861a
Nikola Dimitrovski <dimitrovskin@hotmail.com>
2017-05-05 21:31:04 +0200

    Added warning to n in the for each loop.

--- HelloMiddleEarth/src/HelloMiddleEarth.java
+++ HelloMiddleEarth/src/HelloMiddleEarth.java
@@ -6,7 +6,7 @@ public class HelloMiddleEarth {
         String[] names = { "Bilbo", "Frodo", "Aragorn", "Legolas", "Gandalf"};
         
         //prints the names of the people from middle earth
-        for(String name : names) {
+        for(@SuppressWarnings("unused") String n : names) {
             System.out.println("Hello %s!\n" + names);
         }
 
