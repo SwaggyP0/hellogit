0106995674d98fa50e748f56fdaeeacca2d78c94
Nikola Dimitrovski <dimitrovskin@hotmail.com>
2017-05-05 20:57:24 +0200

    Commited HelloMiddleEarth project.

---
+++ HelloMiddleEarth/src/HelloMiddleEarth.java
@@ -0,0 +1,14 @@
+
+public class HelloMiddleEarth {
+
+    public static void main(String[] args) {
+        
+        String[] names = { "Bilbo", "Frodo", "Aragorn", "Legolas", "Gandalf"};
+        
+        for(int i = 0; i < names.length; i++) {
+            System.out.println("Hello %s!\n" + names[i]);
+        }
+
+    }
+
+}
