7671ca38462c9a64f3e0b9a57cd7fee7c3e60a36
Nikola Dimitrovski <dimitrovskin2@hotmail.com>
2017-05-05 21:26:51 +0200

    Added two new characters.

--- HelloMiddleEarth/src/HelloMiddleEarth.java
+++ HelloMiddleEarth/src/HelloMiddleEarth.java
@@ -3,11 +3,12 @@ public class HelloMiddleEarth {
 
     public static void main(String[] args) {
         
-        String[] names = { "Bilbo", "Frodo", "Aragorn", "Legolas", "Gandalf"};
+        String[] names = { "Bilbo", "Frodo", "Aragorn", "Legolas", "Gandalf", "Talpamir", "Branimir"};
         
         //prints the names of the people from middle earth
-        for(String name : names) {
+        for(int i = 0; i < names.length; i++) {
             System.out.println("Hello %s!\n" + names);
+            System.out.println("How are you doing?");
         }
 
     }
